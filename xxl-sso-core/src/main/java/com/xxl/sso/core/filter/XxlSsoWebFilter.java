0ed08b466b3d5bfc706b27f7570959671fb14cff
Your Name <you@example.com>
2019-04-27 11:54:19 +0800

    xxx

--- xxl-sso-core/src/main/java/com/xxl/sso/core/filter/XxlSsoWebFilter.java
+++ xxl-sso-core/src/main/java/com/xxl/sso/core/filter/XxlSsoWebFilter.java
@@ -73,7 +73,7 @@ public class XxlSsoWebFilter extends HttpServlet implements Filter {
             // redirect logout
             String logoutPageUrl = ssoServer.concat(Conf.SSO_LOGOUT);
             if (logoutRedirect != null && logoutRedirect.trim().length() > 0) {
-                logoutPageUrl = logoutPageUrl.concat("?redirect_url=").concat(logoutRedirect);
+                logoutPageUrl = logoutPageUrl.concat("?redirect_url=").concat(req.getScheme()+"://"+ req.getServerName() + req.getRequestURI() + logoutRedirect);
             }
             res.sendRedirect(logoutPageUrl);
 
